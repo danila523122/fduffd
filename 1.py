cbfed6cc3d83e8eff5c8773db036c549ac48cdf7
danila vechernin <d.vechernin03@gmail.com>
2022-11-29 19:09:54 +0300

    2

--- 1.py
+++ 1.py
@@ -1,12 +1,7 @@
-import sys
-
-from random import choice
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
 
 
-class D(object):
+class Knopka(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
@@ -31,34 +26,3 @@ class D(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "Кнопка"))
-
-
-class Example(QMainWindow, D):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.do_paint = False
-        self.pushButton.clicked.connect(self.paint)
-
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_flag(qp)
-            qp.end()
-
-    def paint(self):
-        self.do_paint = True
-        self.repaint()
-
-    def draw_flag(self, qp):
-        qp.setBrush(QColor(choice(range(0, 256)), choice(range(0, 256)), choice(range(0, 256))))
-        a = choice(range(50, 400))
-        qp.drawEllipse(275, 160, a, a)
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    ex.show()
-    sys.exit(app.exec_())
